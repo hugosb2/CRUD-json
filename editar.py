a6487aab2f4538585468b1f17a53329bcd002c5b
Hugo Barros <hugobs4987@gmail.com>
2025-07-01 12:24:45 -0300

    adição de edição de outros dados

--- editar.py
+++ editar.py
@@ -7,11 +7,33 @@ class editar:
         
         nome_atual = input("Digite o nome atual do usuário que deseja renomear: ")
 
-        novo_nome = input("Digite o novo nome: ")
+        if nome_atual not in usuarios:
+            print("Usuário não encontrado!")
+            return
 
-        usuarios[novo_nome] = usuarios.pop(nome_atual)
+        print("USUÁRIO ENCONTRADO!\nO Que deseja editar?\n\n")
+        print("1. Nome\n2. Idade\n3. Email\n4. Voltar")
+
+        escolha = int(input("Digite a opção aqui: "))
+
+        if escolha == 1:
+            print("Editar nome\n")
+            novo_nome = input("Digite o novo nome: ")
+            usuarios[novo_nome] = usuarios.pop(nome_atual)
+            print("Nome atualizado com sucesso\n\n")
+
+        elif escolha ==2:
+            print("Editar idade\n")
+            usuarios[nome_atual]["idade"] = input("Digite a nova idade: ")
+            print("Idade atualizada com sucesso\n\n")
+
+        elif escolha == 3:
+            print("Editar e-mail\n")
+            usuarios[nome_atual]["email"] = input("Digite o novo email: ")
+            print("E-mail atualizado com sucesso\n\n")
+
+        elif escolha == 4:
+            return
 
         with open ("dados.json", "w", encoding="utf-8") as arquivo:
             json.dump(usuarios, arquivo, indent=4, ensure_ascii=False)
-        
-        print("Dados editados com sucesso!\n\n")
