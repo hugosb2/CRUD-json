5655c9d3543ee17c27a2c58dd87ba64f2cd92c44
Hugo Barros <hugobs4987@gmail.com>
2025-07-01 12:48:02 -0300

    adição de recurso de listar usuários

--- ler.py
+++ ler.py
@@ -5,6 +5,8 @@ class ler:
         with open ("dados.json", "r", encoding="utf-8") as arquivo:
             usuarios = json.load(arquivo)
 
+        print("Total de Usuários: ", len(usuarios), "\n\nLista de usuários: \n")
+
         for nome, dados in usuarios.items():
             print(f"Nome: {nome}, Idade: {dados['idade']}, Email: {dados['email']}")
         print("\n\n")
